4ff4ec31d76ee738e28b8b9265953a6ece74f65c
ljq <296885086@qq.com>
2020-11-26 17:10:39 +0800

    删除

---
+++ 新建文件夹/src/main/java/com/sise/productappriase/ProductappriaseApplication.java
@@ -0,0 +1,13 @@
+package com.sise.productappriase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductappriaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductappriaseApplication.class, args);
+	}
+
+}
